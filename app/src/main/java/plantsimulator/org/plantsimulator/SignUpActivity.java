deba2decb1cacdc8d98de6320cd1fce99f5301b6
opet <opet@Academico.edu>
2018-09-21 11:58:11 -0300

    Initial commit

---
+++ app/src/main/java/plantsimulator/org/plantsimulator/SignUpActivity.java
@@ -0,0 +1,20 @@
+package plantsimulator.org.plantsimulator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void logar(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
